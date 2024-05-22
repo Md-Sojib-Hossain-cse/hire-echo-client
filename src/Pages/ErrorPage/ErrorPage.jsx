a4ed5ea8fd966ea48ddb10cb5cf4e557b01e97ff
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2024-05-23 03:04:37 +0600

    error page added

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,16 @@
+import { MdOutlineHome } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+const ErrorPage = () => {
+    return (
+        <div className="flex flex-col  justify-center items-center h-screen relative">
+            <div className="absolute h-full w-full">
+            <img src="https://i.ibb.co/WcpNMVr/lost-In-Space.jpg" alt="error-page-image" className="h-full w-full object-cover opacity-20 -z-10"/>
+            </div>
+            <img src="https://i.ibb.co/qjgGTCz/page-Not-Found.gif" alt="error-page-image" className="h-1/2 w-auto object-contain rounded-lg z-50"/>
+            <Link to="/" className="btn mt-6 bg-blue-300 hover:bg-blue-400 hover:text-white z-50"><button className="flex justify-center items-center gap-2 font-medium text-lg"><MdOutlineHome className="text-xl"/>Go to Home</button></Link>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Main></Main>,
+        errorElement: <ErrorPage></ErrorPage>
     },
 ]);
 
